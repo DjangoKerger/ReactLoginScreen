b379943b11bd82a0f18d28e9471c49266b00ec8a
“First <Kerger06@aol.com>
2020-09-01 12:27:00 -0500

    Managing complex state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,27 +2,46 @@ import React, { useState } from 'react';
 
 function App(){
 
-    const [name, setName] = useState("");
-    const [completeName, setCompleteName] = useState("")
+    const [fullName, setFullName] = useState({
+        fName: "",
+        lName: ""
+    });
 
-    function handleChange(event){
-        setName(event.target.value);
+    function handleChange(event) {
 
-        event.preventDefault();
+       const {value, name} = event.target;
+       
+       setFullName( prevValue => {
+           if (name === "fName") {
+               return {
+                   fName: value,
+                   lName: prevValue.lName
+               };
+           } else if (name === "lName") {
+               return {
+               fName: prevValue.fName,
+               lName: value
+           };
+        }
+       })
     }
 
-    function handleClick(){
-        setCompleteName(name)
-    }
     return (
         <div className="container">
-            <h1>Hello {completeName}</h1>
-            <form onSubmit={handleClick}>
+            <h1>Hello {fullName.fName} {fullName.lName}</h1>
+            <form>
                 <input
+                    name="fName"
                     onChange={handleChange} 
-                    type="text" 
-                    placeholder="What's your name?"
-                    value={name} 
+                    placeholder="First Name"
+                    value={fullName.fName} 
+                />
+                <input
+                    name="lName"
+                    onChange={handleChange}
+                    placeholder="Last Name"
+                    value={fullName.lName} 
+
                 />
             <button type="submit">Submit</button>
             </form>
